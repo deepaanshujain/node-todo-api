2f45524066dca15e1334cd9f09f64f69b7e508e5
Deepanshu <deejain25@yahoo.com>
2018-11-25 21:16:33 +0530

    Initial Commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,33 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, client) => {
+	if(err) {
+		return console.log("Cannot connect to Mongo DB");
+	}
+	console.log("Connected to Mongo DB");
+
+	const db = client.db('TodoApp');
+
+	// db.collection('Todos').insertOne({
+	// 	text : "To Do 1",
+	// 	completed : false
+	// }, (err, result) => {
+	// 	if(err){
+	// 		return console.log("Something went wrong", err);
+	// 	}
+
+	// 	console.log(JSON.stringify(result.ops, undefined,2));
+	// });
+
+	db.collection('Users').insertOne({
+		name : "Test User",
+		age : 28,
+		location : "New Delhi"
+	}, (err, result) => {
+		if(err) {
+			return console.log("Something went wrong", err);
+		}
+		console.log(JSON.stringify(result.ops, undefined,2));
+	});
+	client.close();
+});
