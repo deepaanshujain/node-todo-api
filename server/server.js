2537c0e2e4f8163b00b41973a8f1e5f7b25a7ff1
Deepaanshu Jain <deepaanshujain@gmail.com>
2018-12-08 20:55:14 +0530

    Get Request by ID

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -28,6 +29,23 @@ app.get('/todos', (req, res) => {
 	}, (e) => {
 		res.status(400).send(e);
 	});
+});
+
+app.get('/todos/:id', (req, res) => {
+	id = req.params.id;
+	if(!ObjectID.isValid(id)) {
+		res.status(404).send();
+	} else {
+		Todo.findById(id).then((todo) => {
+			if(todo) {
+				res.send(todo);
+			} else {
+				res.status(404).send();
+			}
+		}, (e) => {
+			res.status(400).send(e);
+		});
+	}
 });
 
 
