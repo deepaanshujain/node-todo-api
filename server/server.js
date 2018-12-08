212d7c4845ed129b856f8c670a27e49c82d6add3
Deepaanshu Jain <deepaanshujain@gmail.com>
2018-12-08 20:34:00 +0530

    After Get Request

--- server/server.js
+++ server/server.js
@@ -19,7 +19,15 @@ app.post('/todos', (req, res) => {
 		res.send(doc);
 	}, (e) => {
 		res.status(400).send(e);
-	})
+	});
+});
+
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send(todos);
+	}, (e) => {
+		res.status(400).send(e);
+	});
 });
 
 
